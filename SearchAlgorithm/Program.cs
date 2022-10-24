b007610e5633b682554ad541ce968635eb487920
DhioAWP <dhioanugerah0@gmail.com>
2022-10-24 16:50:33 +0700

    Membuat method binary search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -39,5 +39,43 @@ namespace SearchAlgorithm
                 arr[i] = Int32.Parse(s1);
             }
         }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //Accept the number to be Searched
+                Console.Write("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //Apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //Obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //Loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n:");
+                ch = char.Parse(Console.ReadLine());
+            }
+            while ((ch == 'y') || (ch == 'Y'));
+        }
     }
 }
